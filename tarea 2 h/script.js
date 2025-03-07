c921c9aa3a306ca934343ce1712ce63aed57c11b
mar1aguilar <2233034441@correo.ler.uam.mx>
2025-03-07 10:14:49 -0600

    Add files via upload

---
+++ tarea 2 h/script.js
@@ -0,0 +1,8 @@
+let p1= "Pertenezco a la generación que creció con el desarrollo de la tecnología, desde la primaria he interactuado con diferentes páginas web, paginas escolares o de algún registro (digital) y de “redes sociales”. En la secundaria realicé un blog del cual ahora no recuerdo el usuario ni la contraseña, pero aun debe existir en la web. Las tecnologías de programación vistas desde mi perspectiva serán parte de un mundo utópico y también distópico;me hacen creer que existe un espacio intangible, digital en el que se puede inmortalizar mi existencia";
+let p2= "Estoy aprendiendo sobre programación en un área “creativa” y me resulta interesante todo lo que se puede crear.Si mi intención fuera crear un blog y documentar mi vida, ahí existiría, si al formarme como artista y creará una pieza digital, ahí existiría, es otro espacio en el que ahora todos nos conectamos, todos “existen” y todos comparten cualquier tipo de información.";
+
+document.getElementById ('parr1').innerHTML= p1;
+document.getElementById ('parr2').innerHTML= p2;
+
+console.log(p1.length);
+console.log(p2.length);
